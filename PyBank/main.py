088bafae079ef76831e4f5f8753264bba49b1006
Zachary <zach.gibbs@gmail.com>
2020-09-25 22:41:26 -0400

    More work on budget

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,9 +7,34 @@ import csv
 
 csvpath = os.path.join('resources','02-Homework_03-Python_Instructions_Pybank_Resources_Budget_data.csv')
 
-#Open CSV
+#Define Variables
 
-with open(csvpath) as csvfile:
-    csvreader = csv.reader(csvfile, delimiter= ",")
+Dates = []
+Totals = []
+
+#Open CSV File
+
+with open(csvpath, 'r') as csv_file:
+    csvreader = csv.reader(csv_file, delimiter= ",")
+
     for row in csvreader:
-        print(row)
+        Dates.append(row[0])
+        Totals.append(row[1])
+
+print(Dates)
+print(Totals)
+
+
+#Skip Header and define variables and list
+    #Budgetlist = list(csvreader)
+    #print(Budgetlist)
+    #Totalchange = 0
+    #csv_header = next(csv_file)
+    #linecount= 0
+    #Numbmonth= 0
+    
+        #print(csvreader)
+        #Numbmonth += 1
+
+#print(f'There are {Numbmonth} months in this dataset')
+        
