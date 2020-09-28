4045790e760e3ee6dafed3c4b04e65721bfd909e
Zachary <zach.gibbs@gmail.com>
2020-09-28 17:23:22 -0400

    Finally finished pybank

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,31 +1,20 @@
 #Import the modules you need.
-
 import os
 import csv
-
 #Set Path
-
 csvpath = os.path.join('resources','02-Homework_03-Python_Instructions_Pybank_Resources_Budget_data.csv')
-
 #Define Dictionary
-
 Valuedict = {}
 Timedict = {}
-
 #Open CSV File
-
 with open(csvpath, 'r') as csv_file:
     csvreader = csv.reader(csv_file, delimiter= ",")
     csv_header = next(csv_file)
-
 #Iterate and fill both dictionaries necessary
-    
     for row in csvreader:
         Valuedict[row[0]] = int(row[1])
         Timedict[int(row[1])] = row[0]
-
 #Define Values and calculations   
-    
     Profitloss = Valuedict.values()
     Length = len(Valuedict)
     Total = sum(Profitloss)
@@ -34,14 +23,22 @@ with open(csvpath, 'r') as csv_file:
     Minnumber = (min(Profitloss))
     Monthmin = Timedict.get(Minnumber)
     Monthmax = Timedict.get(Largenumber)
-
 #Print Analysis
-
-print('Financial Analysis')
-print('------------------')
-print(f'Total Months: {Length} months')
-print(f'Total: ${Total}')
-print(f'Average Change: ${Average}')
-print(f'Greatest Increase in Profits: {Monthmax} (${Largenumber})')
-print(f'Greatest Decrease in Profits: {Monthmin} (${Minnumber})')
-
+print(f"""Financial Analysis
+------------------
+Total Months: {Length} months
+Total: ${Total}
+Average Change: ${Average}
+Greatest Increase in Profits: {Monthmax} (${Largenumber})
+Greatest Decrease in Profits: {Monthmin} (${Minnumber})"""
+)
+#Write the file
+analysis = open('analysis/Analysis.txt', 'w')
+analysis.write(f"""Financial Analysis
+------------------
+Total Months: {Length} months
+Total: ${Total}
+Average Change: ${Average}
+Greatest Increase in Profits: {Monthmax} (${Largenumber})
+Greatest Decrease in Profits: {Monthmin} (${Minnumber})"""
+)
